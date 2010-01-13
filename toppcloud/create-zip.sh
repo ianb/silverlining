8b9e0637f9c261f02842b6699abae14bcb137f57
Ian Bicking <ianb@colorstudy.com>
2010-01-12 20:23:05 -0600

    Allow a couple env-variable overrides

--- toppcloud/create-zip.sh
+++ toppcloud/create-zip.sh
@@ -2,8 +2,13 @@
 
 pushd "$(dirname $BASH_SOURCE)/../"
 VERSION="$(python setup.py --version)"
-mkdir -p zip-build
-BUILD="zip-build/toppcloud-$VERSION"
+
+if [ -z "$BUILD_DIR" ] ; then
+    BUILD_DIR="zip-build"
+fi
+
+mkdir -p $BUILD_DIR
+BUILD="$BUILD_DIR/toppcloud-$VERSION"
 
 if [ "$VIRTUALENV" = "" ] ; then
     VIRTUALENV="virtualenv"
@@ -31,6 +36,9 @@ rm -r $BUILD/bin $BUILD/include $BUILD/build
 rm -r $BUILD/lib/pip-*.egg/ $BUILD/lib/setuptools.pth $BUILD/lib/setuptools-*.egg $BUILD/lib/easy-install.pth
 find $BUILD -name '*.pyc' -exec rm {} \;
 pushd zip-build
-zip -r toppcloud-${VERSION}.zip toppcloud-${VERSION}
+if [ -z "$ZIP_DIR" ] ; then
+    ZIP_DIR="."
+fi
+zip -r $ZIP_DIR/toppcloud-${VERSION}.zip toppcloud-${VERSION}
 mv toppcloud-${VERSION}.zip ../dist
 popd
