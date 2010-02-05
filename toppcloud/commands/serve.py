8315e91c8ad5f310f131d858ba3071e9f759121b
Ian Bicking <ianb@colorstudy.com>
2010-02-05 02:12:01 -0600

    fix toppcloud serve when bin/python doesn't exist

--- toppcloud/commands/serve.py
+++ toppcloud/commands/serve.py
@@ -1,9 +1,13 @@
 import os
 import sys
 import subprocess
+from cmdutils import CommandError
 
 def command_serve(config):
     dir = os.path.abspath(config.args.dir)
+    if not os.path.exists(os.path.join(config.args.dir, 'app.ini')):
+        raise CommandError(
+            "Could not find app.ini in %s" % config.args.dir)
     if os.path.exists(os.path.join(dir, 'bin', 'python')):
         # We are in a virtualenv situation...
         cmd = [os.path.join(dir, 'bin', 'python'),
@@ -11,8 +15,7 @@ def command_serve(config):
                dir]
     else:
         cmd = [sys.executable,
-               os.path.join(os.path.dirname(__file__),
-                            'devel-runner.py'),
+               os.path.abspath(os.path.join(__file__, '../../devel-runner.py')),
                dir]
     ## FIXME: should cut down the environ significantly
     environ = os.environ.copy()
