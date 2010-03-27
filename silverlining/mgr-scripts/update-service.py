2b89cc41b23e62baa0d513005c2a503f675f08ec
Ian Bicking <ianb@colorstudy.com>
2010-03-27 03:30:23 -0500

    another --clear option

--- silverlining/mgr-scripts/update-service.py
+++ silverlining/mgr-scripts/update-service.py
@@ -12,10 +12,15 @@ Runs any setup necessary for the new instance's services.  Also
 apt-get installs any packages listed in app.ini.
 """)
 
+parser.add_option(
+    '--clear',
+    action='store_true',
+    help='Clear the database')
 
-def update_service(instance_name):
+
+def update_service(instance_name, clear=False):
     app_config = AppConfig.from_instance_name(instance_name)
-    app_config.install_services()
+    app_config.install_services(clear)
     packages = app_config.packages
     if packages:
         print 'Confirming packages installed: %s' % ', '.join(packages)
@@ -24,4 +29,4 @@ def update_service(instance_name):
 if __name__ == '__main__':
     options, args = parser.parse_args()
     instance_name = args[0]
-    update_service(instance_name)
+    update_service(instance_name, options.clear)
