20d546cb9d1a64d5949a37126dfe0d7bf8483a7d
Ian Bicking <ianb@colorstudy.com>
2010-04-05 00:23:30 -0500

    run clear as root

--- silverlining/commands/clear.py
+++ silverlining/commands/clear.py
@@ -4,8 +4,9 @@ from silversupport.appdata import normalize_location
 
 
 def command_clear(config):
+    ## FIXME: this has to be root for bad reasons (it'll try to reinstall the services)
     stdout, stderr, returncode = ssh(
-        'www-mgr', normalize_location(config.args.location)[0],
+        'root', normalize_location(config.args.location)[0],
         '/usr/local/share/silverlining/mgr-scripts/update-service.py --location=%(location)s --clear; '
         'sudo -H -u www-data /usr/local/share/silverlining/mgr-scripts/internal-request.py --update-location %(location)s; '
 
